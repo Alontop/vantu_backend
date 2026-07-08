49fd56c0921ac20fbf9d4c4b911908952ef2583c
alon.607 <alonsovivas2021@gmail.com>
2026-07-08 14:28:19 -0500

    Carga de datos sql para el backend en la nube

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO productos (nombre, precio, color, imagen_url) VALUES
+('VESTIDO CORTO ZW COLLECTION', 179.00, 'NEGRO', '/assets/prod1.png'),
+('BLAZER TRAJE ESTRUCTURA', 519.00, 'BLANCO', '/assets/prod2.png'),
+('ZAPATILLA BAREFOOT', 229.00, 'BLANCO / AZUL', '/assets/prod3.png'),
+('SOMBRERO TÉCNICA SHELL', 159.00, 'ROJO', '/assets/prod4.png'),
+('VESTIDO DRAPEADO', 229.00, 'NEGRO', '/assets/prod5.png'),
+('CHAQUETA BRILLO ZW COLLECTION', 469.00, 'NEGRO', '/assets/prod6.png'),
+('DESTALONADO TACÓN', 169.00, 'GINDA', '/assets/prod7.png'),
+('GAFAS DE SOL OVALADAS', 229.00, 'NEGRO', '/assets/prod8.png');
