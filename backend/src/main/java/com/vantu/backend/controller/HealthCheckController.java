9c8e309326e31bba930f8ca1661d5ba04e943585
Alonso Vivas <117418706+Alontop@users.noreply.github.com>
2026-07-08 20:37:11 -0500

    Agregación de controlador Health para conexion Uptimerobot

---
+++ backend/src/main/java/com/vantu/backend/controller/HealthCheckController.java
@@ -0,0 +1,14 @@
+package com.vantu.backend;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @RequestMapping(value = "/ping", method = {RequestMethod.GET, RequestMethod.HEAD})
+    public ResponseEntity<Void> ping() {
+        return ResponseEntity.ok().build();
+    }
+}
